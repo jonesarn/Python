f6deba31819764c1fd459079bad9965dd0990281
jonesarn <ajones101101@gmail.com>
2026-09-23 15:31:39 -0700

    made in function

--- new.py
+++ new.py
@@ -9,6 +9,8 @@ device = AwsDevice(Devices.Amazon.SV1)
 # cnot which gives two options for the control and target qubits
 
 def bell_state(control, target):
-    bell = Circuit().h(0).cnot(0, target)
+    bell = Circuit().h(0).cnot(control, target)
     task = device.run(bell, shots=100)
     return task.result().measurement_counts
+
+bell_state(0, 1)
